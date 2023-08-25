f4d0ff80b29003872a20d9890664c09f76403953
Gerson Moises Reyaga Sotelo <gerson.reynaga23@gmail.com>
2023-08-24 22:21:17 -0500

    actualizacion

---
+++ index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+ScrollReveal().reveal(".right", {
+  duration: 2000,
+  origin: "right",
+  distance: "100px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".left", {
+  duration: 2000,
+  origin: "left",
+  distance: "100px",
+  reset: true,
+});
+
+ScrollReveal().reveal(".yo", {
+  duration: 2000,
+  origin: "left",
+  distance: "100px",
+});
+
+ScrollReveal().reveal(".yoo", {
+  duration: 2000,
+  origin: "right",
+  distance: "100px",
+});
